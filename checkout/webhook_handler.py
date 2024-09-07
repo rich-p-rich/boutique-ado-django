00e197a50e60569b12def107d3c48a2bc61aceb0
Richard Ricot <richricot@gmail.com>
2024-09-07 17:04:58 +0000

    Product Admin - add product view, url and template

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -75,7 +75,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    email=billing_details.email,
+                    email=shipping_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
                     postcode=shipping_details.address.postal_code,
